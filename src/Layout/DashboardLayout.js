a3c045c777a3d94a43166d9f8d46368f993b3ae3
Md.Al kafee <sojeebahmed.sa@gmail.com>
2022-11-29 23:43:44 +0600

    seller route only

--- src/Layout/DashboardLayout.js
+++ src/Layout/DashboardLayout.js
@@ -2,11 +2,13 @@ import React, { useContext } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import { AuthContext } from '../contexts/AuthProvider';
 import useAdmin from '../hooks/useAdmin';
+import useSeller from '../hooks/useSeller';
 import Navbar from '../Pages/Shared/Navbar/Navbar';
 
 const DashboardLayout = () => {
     const { user } = useContext(AuthContext);
     const [isAdmin] = useAdmin(user?.email)
+    const [isSeller] = useSeller(user?.email)
 
     return (
         <div>
@@ -22,12 +24,22 @@ const DashboardLayout = () => {
                     <ul className="menu p-4 w-80 bg-slate-200 text-base-content">
                         {/* <!-- Sidebar content here --> */}
                         <li><Link to='/dashboard'>My Orders</Link></li>
+
+                        {
+                            isSeller && <>
+                                <li><Link to='/dashboard/addaproduct'>Add Product</Link></li>
+                                <li><Link to='/dashboard/myproducts'>My Products</Link></li>
+
+                            </>
+                        }
+
+
+
                         {
                             isAdmin && <>
                                 <li><Link to='/dashboard/allusers'>All Buyer</Link></li>
                                 <li><Link to='/dashboard/allseller'>All Seller</Link></li>
-                                <li><Link to='/dashboard/addaproduct'>Add Product</Link></li>
-                                <li><Link to='/dashboard/myproducts'>My Products</Link></li>
+
                             </>
                         }
 
