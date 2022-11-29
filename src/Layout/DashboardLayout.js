c2bd98229919241c6c9007dde0bea40921bd684d
Md.Al kafee <sojeebahmed.sa@gmail.com>
2022-11-30 00:01:02 +0600

    buyer obly route added

--- src/Layout/DashboardLayout.js
+++ src/Layout/DashboardLayout.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import { AuthContext } from '../contexts/AuthProvider';
 import useAdmin from '../hooks/useAdmin';
+import useBuyer from '../hooks/useBuyer';
 import useSeller from '../hooks/useSeller';
 import Navbar from '../Pages/Shared/Navbar/Navbar';
 
@@ -9,6 +10,7 @@ const DashboardLayout = () => {
     const { user } = useContext(AuthContext);
     const [isAdmin] = useAdmin(user?.email)
     const [isSeller] = useSeller(user?.email)
+    const [isBuyer] = useBuyer(user?.email)
 
     return (
         <div>
@@ -23,8 +25,13 @@ const DashboardLayout = () => {
                     <label htmlFor="dashboard-drawer" className="drawer-overlay"></label>
                     <ul className="menu p-4 w-80 bg-slate-200 text-base-content">
                         {/* <!-- Sidebar content here --> */}
-                        <li><Link to='/dashboard'>My Orders</Link></li>
 
+
+                        {
+                            isBuyer && <>
+                                <li><Link to='/dashboard'>My Orders</Link></li>
+                            </>
+                        }
                         {
                             isSeller && <>
                                 <li><Link to='/dashboard/addaproduct'>Add Product</Link></li>
